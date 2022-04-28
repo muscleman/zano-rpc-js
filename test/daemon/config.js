c3dd27ab87bdc80ba3e6c7ca01d08a7d83a9fae8
muscleman <musclesonvacation@hotmail.com>
2022-04-27 20:36:26 -0500

    started implementing daemon calls

--- test/daemon/config.js
+++ test/daemon/config.js
@@ -1,12 +1,14 @@
 'use strict'
 
 exports = module.exports = {
-  daemonAddress: process.env.DAEMON_ADDRESS || 'http://127.0.0.1:39994',
+  daemonAddress: process.env.DAEMON_ADDRESS || 'http://10.0.0.13:12111',
   daemonUsername: process.env.DAEMON_USER || 'user',
   daemonPassword: process.env.DAEMON_PASSWORD || 'pass',
-  blockHash: '9398b5cc30d28d282316e70e98efab2c569e9fac8c88ca5dad4a58555401b3da',
-  blockHeight: 1,
-  stagenetWalletAddress: 'as2uPW7Te7MGTZVxhSmxN4hqugxrgHdRgEBcz6ci4ewFKSHhEFyP6mJBrn52p6g8pyD92bkgGX9f8YkjN1VALvgF1yFx8BaQ2',
+  blockHash: '6d420b6a27ac284a93b86b6bd52423adc29f63c958d641c12eced1cd523f12e9',
+  blockHeight: 100,
+  stagenetWalletAddress: 'ZxBj1cQSiCfhQKHvZF8nFtM6dgEn2htg66p3Z9BUhVNmivASMwhX2dbN52gMLq9CKs38vTwMRJccRA4MeLqrQEGg1bWpjgQfQ',
   txids: ['8d63cfb68c053f2bcfe54d8792a24dbb46df15399f18c9814e2aa914c5dabc3b'],
-  spent_key: '8618320bf108e2827f927c7f7300b47bc21ca7708fd800b09a74070face15e3d'
+  spent_key: '8618320bf108e2827f927c7f7300b47bc21ca7708fd800b09a74070face15e3d',
+  alias: 'muscleonvacation',
+  aliasFee: 100000000000
 }
