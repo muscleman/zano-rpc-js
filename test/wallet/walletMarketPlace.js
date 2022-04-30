74ee3f049abb46c1cac03714694a02abd56194d7
muscleman <musclesonvacation@hotmail.com>
2022-04-29 20:12:04 -0500

    marketplace WIP

--- test/wallet/walletMarketPlace.js
+++ test/wallet/walletMarketPlace.js
@@ -21,39 +21,95 @@ describe('RPCWallet marketplace tests', () => {
                         })
     walletClient.sslRejectUnauthorized(false)
 
-    before(async function() {
-        try {
-            
-        } catch (error) {
-            
-        }
+    beforeEach(async function() {
+        try {} catch (error) {}
     })
     after(async function() {
-        try {
-            
-        } catch (error) {
-            
-        }
+        try {} catch (error) {}
     })
-    xit('marketplace_get_offers_ex', () => {
+    it('marketplace_get_offers_ex', () => {
+        return expect(walletClient.marketplace_get_offers_ex())
+        .to.eventually.satisfy(function(response) {
+            if ('total_transfers' in response)
+                return expect(response).to.have.keys('tx_hash', 'tx_blob_size', 'status', 'total_transfers', 'total_offers')
+            else
+                return expect(response).to.have.keys('status', 'total_offers')
+                        .and.to.have.property('total_offers', 0)
+          })
+    })
+    it('marketplace_push_offer', () => {
         const opts = {
-          od: {
-    
-          }
+            od: {
+                // tx_hash: '',
+                // tx_original_hash: '',
+                // index_in_tx: '',
+                // timestamp: 0,
+                fee: config.units,
+                // security: '',
+                ot: 1,
+                ap: '20',
+                at: '1',
+                // b:
+                t: 'T-shirt with Zano logo',
+                // p
+                lco: 'World Wide',
+                lci: '',
+                cnt: 'Skype: some_skype, discord: some_user#01012',
+                com: 'A comment about the tshirt',
+                pt: 'Credit cards, BTC, ZANO, ETH',
+                do: 'Additional conditions',
+                cat: 'CLS:MAN:TSH',
+                et: 10,
+                url: ''
+            }
         }
-        return expect(walletClient.marketplace_get_offers_ex(opts))
-          .to.eventually.have.keys('tx_hash', 'tx_blob_size')
+        return expect(walletClient.marketplace_push_offer(opts))
+                    .to.eventually.have.keys('tx_hash', 'tx_blob_size')
     })
-    xit('marketplace_push_offer', () => {
-        return expect(walletClient.marketplace_push_offer({}))
-          .to.eventually.have.keys('tx_hash', 'tx_blob_size')
+    it('marketplace_push_update_offer', async () => {
+
+        const opts = {
+            od: {
+                // tx_hash: '',
+                // tx_original_hash: '',
+                // index_in_tx: '',
+                // timestamp: 0,
+                fee: config.units,
+                // security: '',
+                ot: 1,
+                ap: '20',
+                at: '1',
+                // b:
+                t: 'T-shirt with Zano logo',
+                // p
+                lco: 'World Wide',
+                lci: '',
+                cnt: 'Skype: some_skype, discord: some_user#01012',
+                com: 'A comment about the tshirt',
+                pt: 'Credit cards, BTC, ZANO, ETH',
+                do: 'Additional conditions',
+                cat: 'CLS:MAN:TSH',
+                et: 10,
+                url: ''
+            }
+        }
+        let offer = await walletClient.marketplace_push_offer(opts)
+        console.log('waiting for confirmations')
+        await waitForBlocks(1000 * 120)
+        opts.od.lco = 'Europe'
+        opts.tx_id = offer.tx_hash
+            
+        offer = await walletClient.marketplace_push_update_offer(opts)
+        opts.tx_id = offer.tx_hash
+        return expect(1).to.equal(1)
+        // console.log('waiting for confirmations')
+        // await waitForBlocks(1000 * 60)
+        // return expect(walletClient.marketplace_cancel_offer(opts))
+        //             .to.eventually.have.keys('tx_hash', 'tx_blob_size')
+
     })
-    xit('marketplace_push_update_offer', () => {
-        return expect(walletClient.marketplace_push_update_offer({}))
-          .to.eventually.have.keys('tx_hash', 'tx_blob_size')
-    })
-    xit('marketplace_cancel_offer', () => {
-        return expect(walletClient.marketplace_cancel_offer({}))
-            .to.eventually.have.keys('tx_hash', 'tx_blob_size')
-    })
+
+    function waitForBlocks(numberOfBlocks) {
+        return new Promise(resolve => setTimeout(resolve, numberOfBlocks))
+    }
 })
